dc3f84085858cf991af1c9e36d4aa1b6b9209c2d
Cynthia Kiser <cynthiakiser@gmail.com>
2018-09-13 16:06:48 -0700

    Made the carousel controls slide the set of thumbnails right or left

--- carousel-with-bottom-thumbnails/carousel.js
+++ carousel-with-bottom-thumbnails/carousel.js
@@ -23,18 +23,48 @@
       $('.carousel-wbt__thumbnail-image', block).css('width', thumbnail_width(block) + 'px');
     }
 
-    function slide_thumbnails(to_item, from_item) {
-      // If one of the controls is clicked and it leads to an extant thumbnail, we slide the thumbnail-wrapper an
-      // appropriate distance and set the item being slid away from as "animating" until the animation is done.
-      if (to_item.length) {
-        var wrapper = to_item.parent();
-        wrapper.css('transform', 'translateX(' + get_translate_distance_for_centering(to_item) + 'px)');
-        to_item.addClass('active');
-        from_item.removeClass('active').addClass('animating');
-        wrapper.on('transitionend', function() {
-          from_item.removeClass('animating');
+    function slide_thumbnails(block, direction) {
+      // When one of the controls is clicked, we check to see if there are any more images to show if we slide
+      // in the requested direction. If so, we slide the thumbnail-slider within the thumbnail-wrapper by updating
+      // the translateX property. This function returns the value we need for the translateX.
+      var slider = $('.js-thumbnail-slider', block);
+      var current_translate_value = slider.attr('style').split('(')[1].split(')')[0].replace('px', '');
+      var slider_left_edge = slider.offset().left;
+
+      // Default is for the slider not to move
+      var position = current_translate_value;
+
+      if (direction == 'left') {
+        // Loop through the images backwards until we find the first image that is scrolled out of site to the left
+        // and return the position of its left edge. If there are no images out the left hand side, then we will just
+        // return the current_translate_value, aaka stay where we are.
+        $($('.carousel-wbt__thumbnail', slider).get().reverse()).each(function(index, element) {
+          var thumbnail_left_edge = $(element).offset().left - slider_left_edge;
+          if (-thumbnail_left_edge > current_translate_value) {
+            position = thumbnail_left_edge;
+            return false;
+          }
         });
+      } else if (direction == 'right') {
+        // If we have images hanging out the right hand boundary, then we need to update translate when left arrow clicked
+        // If not, then we will return the current_translate_value (aka stay where we are).
+        var last_thumbnail = $('.carousel-wbt__thumbnail', block).eq(-1);
+        var slider_right_edge = last_thumbnail.offset().left + last_thumbnail.outerWidth();
+        var wrapper = $('.carousel-wbt__thumbnail-wrapper');
+        var wrapper_right_edge = wrapper.offset().left + wrapper.outerWidth();
+
+        if (slider_right_edge > wrapper_right_edge) {
+          $('.carousel-wbt__thumbnail', slider).each(function(index, element) {
+            var thumbnail_left_edge = $(element).offset().left - slider_left_edge;
+            if (-thumbnail_left_edge < current_translate_value) {
+              position = -thumbnail_left_edge;
+              return false;
+            }
+          });
+        }
       }
+
+      return position;
     }
 
     $('.carousel-wbt').each(function() {
@@ -50,13 +80,11 @@
 
       // Set up the carousel controls.
       $('.js-cwbt-control-right', block).on('click', function() {
-        console.log('.js-cwbt-control-right');
-        var offset = 192;
+        var offset = slide_thumbnails(block, 'right');
         $('.js-thumbnail-slider', block).css('transform', 'translateX(' + offset + 'px)');
       });
       $('.js-cwbt-control-left', block).on('click', function() {
-        console.log('.js-cwbt-control-left');
-        var offset = 192;
+        var offset = slide_thumbnails(block, 'left');
         $('.js-thumbnail-slider', block).css('transform', 'translateX(-' + offset + 'px)');
       });
 
