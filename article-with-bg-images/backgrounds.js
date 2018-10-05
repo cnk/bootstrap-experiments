d707e6d691c11cd080bc963c730d3a7185461515
Cynthia Kiser <cynthiakiser@gmail.com>
2018-10-04 21:53:54 -0700

    Removed bootstrap classes in html - confusing to have some styles there and some in css file AND I wanted more control over margin sizes
    Adjusted where the image transition occures
    
    Added overlay when text is in view

--- article-with-bg-images/backgrounds.js
+++ article-with-bg-images/backgrounds.js
@@ -2,23 +2,31 @@
 
 $(function() {
   $(window).scroll(function() {
+    var img_transition_offset = 75;
     var scroll_top = $(window).scrollTop();
     var scroll_bottom = $(document).height() - $(window).height() - $(window).scrollTop();
     var current_bgimg = $('.bgimg.current');
     var prev_bgimg = $('.bgimg.current').prevAll('.bgimg').first();
     var next_bgimg = $('.bgimg.current').nextAll('.bgimg').first();
 
-    if (next_bgimg.length && 
-        (scroll_top + $(window).height() >= $('.bgimg__text', next_bgimg).offset()['top'])) {
-    // When the full title is visible / when the text scrolls onto the bottom of the window, change bg image
+    // When the title is visible change bg image
+    if (next_bgimg.length &&
+        (scroll_top + $(window).height() + img_transition_offset >= $('.bgimg__text', next_bgimg).offset()['top'])) {
       $('body').css('background-image', 'url(' + next_bgimg.data('bgimg') + ')');
       current_bgimg.removeClass('current');
       next_bgimg.addClass('current');
-    } else if (prev_bgimg.length && (scroll_top + $(window).height() <= current_bgimg.offset()['top'])) {
+    } else if (prev_bgimg.length &&
+               (scroll_top + $(window).height() - img_transition_offset <= current_bgimg.offset()['top'])) {
       $('body').css('background-image', 'url(' + prev_bgimg.data('bgimg') + ')');
       current_bgimg.removeClass('current');
       prev_bgimg.addClass('current');
     }
 
+    if ($('.bgimg__text', current_bgimg).length &&
+        scroll_top + $(window).height() >= $('.bgimg__text', current_bgimg).offset()['top'] + 20) {
+      $('.pages').css('background-color', 'rgb(0,0,0,0.5)')
+    } else {
+      $('.pages').css('background-color', 'rgb(0,0,0,0)')
+    }
   });
 })
