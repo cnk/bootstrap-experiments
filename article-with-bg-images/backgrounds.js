d8fa4a1b6893a2c89c9ab2c3e29f28ebef42205c
Cynthia Kiser <cynthiakiser@gmail.com>
2018-10-04 22:00:58 -0700

    changed main class name because the chunks are really pages - bgimg is incidental

--- article-with-bg-images/backgrounds.js
+++ article-with-bg-images/backgrounds.js
@@ -5,25 +5,25 @@ $(function() {
     var img_transition_offset = 75;
     var scroll_top = $(window).scrollTop();
     var scroll_bottom = $(document).height() - $(window).height() - $(window).scrollTop();
-    var current_bgimg = $('.bgimg.current');
-    var prev_bgimg = $('.bgimg.current').prevAll('.bgimg').first();
-    var next_bgimg = $('.bgimg.current').nextAll('.bgimg').first();
+    var current_page = $('.page.current');
+    var prev_page = $('.page.current').prevAll('.page').first();
+    var next_page = $('.page.current').nextAll('.page').first();
 
     // When the title is visible change bg image
-    if (next_bgimg.length &&
-        (scroll_top + $(window).height() + img_transition_offset >= $('.bgimg__text', next_bgimg).offset()['top'])) {
-      $('body').css('background-image', 'url(' + next_bgimg.data('bgimg') + ')');
-      current_bgimg.removeClass('current');
-      next_bgimg.addClass('current');
-    } else if (prev_bgimg.length &&
-               (scroll_top + $(window).height() - img_transition_offset <= current_bgimg.offset()['top'])) {
-      $('body').css('background-image', 'url(' + prev_bgimg.data('bgimg') + ')');
-      current_bgimg.removeClass('current');
-      prev_bgimg.addClass('current');
+    if (next_page.length &&
+        (scroll_top + $(window).height() + img_transition_offset >= $('.page__text', next_page).offset()['top'])) {
+      $('body').css('background-image', 'url(' + next_page.data('bgimg') + ')');
+      current_page.removeClass('current');
+      next_page.addClass('current');
+    } else if (prev_page.length &&
+               (scroll_top + $(window).height() - img_transition_offset <= current_page.offset()['top'])) {
+      $('body').css('background-image', 'url(' + prev_page.data('bgimg') + ')');
+      current_page.removeClass('current');
+      prev_page.addClass('current');
     }
 
-    if ($('.bgimg__text', current_bgimg).length &&
-        scroll_top + $(window).height() >= $('.bgimg__text', current_bgimg).offset()['top'] + 20) {
+    if ($('.page__text', current_page).length &&
+        scroll_top + $(window).height() >= $('.page__text', current_page).offset()['top'] + 20) {
       $('.pages').css('background-color', 'rgb(0,0,0,0.5)')
     } else {
       $('.pages').css('background-color', 'rgb(0,0,0,0)')
